0bd5a40e2825930f96bcf74d2e12372d1af0383c
Yuriy <yuriy@webdwarves.com>
2017-06-12 21:14:58 +0200

    UserSiteDetailsWithStats model

--- db/sites.ts
+++ db/sites.ts
@@ -8,3 +8,15 @@ export class UserSiteDetails extends BaseSite {
   siteAccessLevel: number = 0;
   mapProviderId: number = 0;
 }
+
+export class UserSiteDetailsWithStats extends UserSiteDetails {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  deviceMode: number;
+  defaultMapProviderId: number;
+  timezone: string;
+  userCount: number;
+  deviceCount: number;
+  alarmCount: number;
+}
