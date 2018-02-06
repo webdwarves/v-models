c4f6dcdb41354d7de95828b72a86b99406989143
pawel <pawel@dev.com>
2018-02-06 17:59:28 +0100

    deviceClassId in AlarmState

--- db/alarm.ts
+++ db/alarm.ts
@@ -14,6 +14,7 @@ export enum InAlarmStateEnum {
 export class AlarmState {
   deviceId: number;
   deviceName: string;
+  deviceClassId: number;
   eventTypeId: number;
   eventTypeName: string;
   alarmOnState: AlarmOnStateEnum;
