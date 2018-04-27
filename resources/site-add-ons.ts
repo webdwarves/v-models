49712c4b6cb4b6bcf82d84d6ea75b8ba026d08e0
pawel <pawel@dev.com>
2018-04-27 15:15:04 +0200

    add ons resource updated

--- resources/site-add-ons.ts
+++ resources/site-add-ons.ts
@@ -3,9 +3,11 @@ import {
   SiteDeviceClass,
   SiteDeviceAlarm
 } from '../db/site-add-on-functionality-settings';
+import { SiteDetailsWithStats } from '../db/sites';
 
 export class SiteAddOnsResourceModel {
   details: SiteAddOnFunctionalitySettings[] = [new SiteAddOnFunctionalitySettings()];
   availableClasses: SiteDeviceClass[] = [new SiteDeviceClass()];
   availableAlarms: SiteDeviceAlarm[] = [new SiteDeviceAlarm()];
+  siteDetails: SiteDetailsWithStats = new SiteDetailsWithStats();
 }
