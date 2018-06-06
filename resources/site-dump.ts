98e2af8f003f4e5067256094aa79de10b6fc9614
pawel <pawel@dev.com>
2018-06-06 19:25:32 +0200

    map details & addons added to the site dump

--- resources/site-dump.ts
+++ resources/site-dump.ts
@@ -1,7 +1,11 @@
 import { DeviceAlarm, DeviceAlarmSuppressions, DeviceConfig, DeviceInfo, DeviceLocation } from '../db/device';
+import { Grouping } from '../db/grouping';
 import { Permission } from '../db/permissions';
+import { SiteAddOnFunctionalitySettings, SiteDeviceAlarm, SiteDeviceClass } from '../db/site-add-on-functionality-settings';
 import { SiteDetails } from '../db/sites';
+import { Structure } from '../db/structures';
 import { UserDetails } from '../db/users';
+import { Zone } from '../db/zone';
 
 export interface SiteDumpResourceModel {
   user: UserDetails;
@@ -19,5 +23,15 @@ export interface SiteDumpResourceModel {
       alarms: DeviceAlarm[];
       alarmsSuppresions: DeviceAlarmSuppressions[];
     }
+  }
+  addOns: {
+    details: SiteAddOnFunctionalitySettings[]
+    availableClasses: SiteDeviceClass[]
+    availableAlarms: SiteDeviceAlarm[]
+  },
+  map: {
+    groupings: Grouping[],
+    structures: Structure[],
+    zones: Zone[]
   }
 }
