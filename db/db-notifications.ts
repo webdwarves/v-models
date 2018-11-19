151b981cb59fd29a9ae3cdfc71aaa8fe6f97562c
pawel <pawel@dev.com>
2018-11-19 17:45:55 +0100

    CameraQualityIndex type

--- db/db-notifications.ts
+++ db/db-notifications.ts
@@ -7,6 +7,8 @@ export interface CameraDeviceDBNotification {
   cameraDeviceId: number
 }
 
+export type CameraQualityIndex = -1 | 1 | 2 | 3 | 4 | 5 | 6 | 7
+
 export interface CameraQualityIndexUpdate extends SiteDBNotification, CameraDeviceDBNotification {
-  qualityIndex: -1 | 1 | 2 | 3 | 4 | 5 | 6 | 7
+  qualityIndex: CameraQualityIndex
 }
